17c58a8c007d38feaa2203249f8d4d9671ba91ef
ken0901 <ken5260715@gmail.com>
2023-04-28 21:07:47 -0700

    Adding firebase login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,40 +1,56 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
-interface AuthResponseData{
+export interface AuthResponseData{
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
     constructor(private http: HttpClient){}
     
-    baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_zawNnlG2Hi0tU2kxCErZrux4j89NvAk';
+    API_KEY = '';
+    baseUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+    signUpUrl = 'signUp?key='
+    signInUrl= 'signInWithPassword?key=';
 
     signUp(email: string, password: string){
-        return this.http.post<AuthResponseData>(this.baseUrl,
+        return this.http.post<AuthResponseData>(this.baseUrl + this.signUpUrl + this.API_KEY,
             {
                 email: email,
                 password: password,
                 returnSecureToken: true    
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'An unknown error occurred!';
-            if(!errorRes.error || !errorRes.error.error){
-                return throwError(errorMessage);
+        ).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>(this.baseUrl + this.signInUrl + this.API_KEY,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true  
             }
-            switch(errorRes.error.error.message){
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email exists already';
-            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage = 'An unknown error occurred!';
+        if(!errorRes.error || !errorRes.error.error){
             return throwError(errorMessage);
-        }));
+        }
+        switch(errorRes.error.error.message){
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
     }
 }
