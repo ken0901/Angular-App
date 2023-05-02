d121d99978ce59321c2c36bd80fa7a3642c70504
ken0901 <ken5260715@gmail.com>
2023-05-01 21:14:51 -0700

    Adding autoLogin, AuthLogout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData{
 @Injectable({providedIn: 'root'})
 export class AuthService{
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer;
 
     constructor(private http: HttpClient,
                 private router: Router){}
@@ -54,12 +55,51 @@ export class AuthService{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration: number){
+        console.log(expirationDuration);
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration)
+    }
+
+    autoLogin(){
+        const userData:{
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email, 
+            userData.id, 
+            userData._token, 
+            new Date(userData._tokenExpirationDate
+        ));
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
+        }
     }
 
     private handleAuthentication(resData){
         const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
         const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
         this.user.next(user);
+        this.autoLogout(resData.expiresIn * 1000);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse){
