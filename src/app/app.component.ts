1e4febbbc6808190b7e37731a3187bd31fb92712
ken0901 <ken5260715@gmail.com>
2023-05-08 21:21:13 -0700

    Angular Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { LoggingService } from './services/logging.service';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('divState',[
+      state('normal',style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transForm: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent implements OnInit{
-  constructor(private authService: AuthService,
-              private loggingService: LoggingService){}
   
-  ngOnInit(): void {
-    this.authService.autoLogin();
-    this.loggingService.printLog('Hello From AppComponent ngOnInit');
+  constructor(private authService: AuthService,
+    private loggingService: LoggingService){}
+    
+    ngOnInit(): void {
+      this.authService.autoLogin();
+      this.loggingService.printLog('Hello From AppComponent ngOnInit');
+    }
+  
+  // Angular Animation project
+  list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+  onAdd(item) {
+    this.list.push(item);
+  }
+  onShrink() {
+  }
+  onAnimate() {
   }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
         HttpClientModule,
         SharedModule,
         CoreModule,
+        BrowserAnimationsModule  
         // StandAloneModule,
     ],
     bootstrap: [AppComponent],
